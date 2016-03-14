79a74950db8ff8d9d9b88806f7ac5da1ede6f506
Nicolas Claverie <info@artscore-studio.fr>
2016-03-14 09:26:37 +0100

    Fix typo in namespace

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -7,7 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace ASF\LayoutBundle\Composer;
+namespace ASF\SchedulerBundle\Composer;
 
 use Composer\Script\CommandEvent;
 use Symfony\Component\Process\Process;
