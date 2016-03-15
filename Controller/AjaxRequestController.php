b7850c11aa4a596f5be597ba0e0cd130ff2aeed8
Nicolas Claverie <info@artscore-studio.fr>
2016-03-15 13:39:30 +0100

    Add ajax request for resize event on Fullcalendar.io

--- Controller/AjaxRequestController.php
+++ Controller/AjaxRequestController.php
@@ -32,14 +32,11 @@ class AjaxRequestController extends Controller
      */
     public function loadCalendarAction(Request $request)
     {
-        $startDatetime = new \DateTime();
-        $startDatetime->setTimestamp($request->get('start'));
-        
-        $endDatetime = new \DateTime();
-        $endDatetime->setTimestamp($request->get('start'));
+        $startDatetime = new \DateTime($request->get('start'));
+        $endDatetime = new \DateTime($request->get('end'));
         
         $events = $this->container->get('event_dispatcher')->dispatch(
-            CalendarEvents::CONFIGURE, 
+            CalendarEvents::LOAD_EVENTS, 
             new CalendarEvent($startDatetime, $endDatetime, $request
         ))->getEvents();
         
@@ -55,5 +52,36 @@ class AjaxRequestController extends Controller
         $response->setContent(json_encode($return_events));
         
         return $response;
+    }
+    
+    public function eventResizeAction(Request $request)
+    {
+    	$response = new Response();
+    	$response->headers->set('Content-Type', 'application/json');
+    	
+    	$entityManager = $this->get('asf_scheduler.calendar_event.manager');
+    	
+    	$id = $request->get('id');
+    	$startedAt = new \DateTime($request->get('startedAt'));
+    	$finishedAt = new \DateTime($request->get('finishedAt'));
+    	$isAllDay = $request->get('isAllDay');
+    	$return_value = array();
+    	try {
+    		$event = $entityManager->getRepository()->findOneBy(array('id' => $id));
+    		if ( is_null($event) ) {
+    			throw new \Exception(sprintf('The vent with id "%s" not found.', $id));
+    		}
+    		
+    		$event->setStartedAt($startedAt)->setFinishedAt($finishedAt)->setIsAllDay($isAllDay);
+    		$entityManager->getEntityManager()->flush();
+    		
+    		$return_value['msg'] = $this->get('translator')->trans('The event %name% has been successfully saved.', array('%name%' => $event->getTitle(), 'asf_scheduler'));
+    		
+    	} catch (\Exception $e) {
+    		$return_value['msg'] = $e->getMessage();
+    	}
+    	
+    	$response->setContent(json_encode($return_value));
+    	return $response;
     }
 }
