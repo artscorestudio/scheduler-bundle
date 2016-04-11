ed26fb33c153783ed159fd751abe118fdc830cd7
Nicolas Claverie <info@artscore-studio.fr>
2016-04-11 16:35:23 +0200

    Change use statement to ASFEntityManager

--- Entity/Manager/ASFSchedulerManager.php
+++ Entity/Manager/ASFSchedulerManager.php
@@ -9,7 +9,7 @@
  */
 namespace ASF\SchedulerBundle\Entity\Manager;
 
-use ASF\CoreBundle\Entity\Manager\ASFEntityManager;
+use ASF\CoreBundle\Utils\Manager\ASFEntityManager;
 
 /**
  * Generic Entity Manager for this bundle
